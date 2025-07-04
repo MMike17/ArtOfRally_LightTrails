21d8b41b4a576fe741591b52cdb1bbdd432ecded
MMike17 <matthews.r.mike@gmail.com>
2025-07-04 18:31:08 +0200

    Add control scripts on car

--- Patches.cs
+++ Patches.cs
@@ -2,26 +2,41 @@ using HarmonyLib;
 
 namespace LightTrails
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
-	
-	//	this will negate the method
-	//  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-	//  	{
-	//      	foreach (var instruction in instructions)
-	//          	yield return new CodeInstruction(OpCodes.Ret);
-	//  	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // [HarmonyPatch(typeof(), MethodType.)]
+    // static class type_method_Patch
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    //	this will negate the method
+    //  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //  	{
+    //      	foreach (var instruction in instructions)
+    //          	yield return new CodeInstruction(OpCodes.Ret);
+    //  	}
+
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
+
+    [HarmonyPatch(typeof(PlayerManager), "Init")]
+    static class TrailsSpawner
+    {
+        public static ConditionTypes.Weather weather;
+
+        static void Postfix(PlayerManager __instance, ConditionTypes.Weather _weather)
+        {
+            weather = _weather;
+
+            BrakeEffects brakeEffects = __instance.PlayerObject.GetComponentInChildren<BrakeEffects>();
+            brakeEffects.LeftBrakeLightTransform.gameObject.AddComponent<TrailAnimator>();
+            brakeEffects.RightBrakeLightTransform.gameObject.AddComponent<TrailAnimator>();
+        }
+    }
 }
